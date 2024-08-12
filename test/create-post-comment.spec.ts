ddbc166d4d0e86fd4519552c1062df9248e0256e
brunorg21 <rbrunog21@gmail.com>
2024-08-11 22:42:17 -0300

    feat: add create post comment and create post test

---
+++ test/create-post-comment.spec.ts
@@ -0,0 +1,23 @@
+import { Post } from "@/domain/blog/enterprise/entities/post";
+import { PostComment } from "@/domain/blog/enterprise/entities/post-comment";
+import { describe, expect, it } from "vitest";
+
+describe("create post comment on post", () => {
+  it("should be able to create a post comment in a post", () => {
+    const post = Post.create({
+      authorId: "author-id",
+      communityId: "community-id",
+      content: "teste",
+      title: "Teste",
+      attachments: [],
+    });
+
+    const postComment = PostComment.create({
+      authorId: "user-id",
+      content: "comment",
+      postId: post._id,
+    });
+
+    expect(postComment.postId).toBe(post._id);
+  });
+});
---
+++ test/create-post.spec.ts
@@ -0,0 +1,16 @@
+import { Post } from "@/domain/blog/enterprise/entities/post";
+import { describe, expect, it } from "vitest";
+
+describe("create post", () => {
+  it("should be able to create a post", () => {
+    const post = Post.create({
+      authorId: "author-id",
+      communityId: "community-id",
+      content: "teste",
+      title: "Teste",
+      attachments: [],
+    });
+
+    expect(post.title).toBe("Teste");
+  });
+});
