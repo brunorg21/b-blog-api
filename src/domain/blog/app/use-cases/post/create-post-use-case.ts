9c3d263669610d415837d20ee476e4df15d8a1de
brunorg21 <rbrunog21@gmail.com>
2024-09-03 16:55:20 -0300

    feat: add create and update use case

--- src/domain/blog/app/use-cases/post/create-post-use-case.ts
+++ src/domain/blog/app/use-cases/post/create-post-use-case.ts
@@ -1,9 +1,33 @@
 import { Post } from "@/domain/blog/enterprise/entities/post";
 import { PostRepository } from "../../repositories/post-repostitory";
 
+interface CreatePostUseCaseRequest {
+  title: string;
+  content: string;
+  authorId: string;
+  bloggersCommunityId: string | null;
+  topics: string[];
+}
+
+interface CreatePostUseCaseResponse {
+  post: Post;
+}
+
 export class CreatePostUseCase {
   constructor(private readonly postRepository: PostRepository) {}
-  async execute(post: Post): Promise<void> {
-    await this.postRepository.save(post);
+  async execute(
+    post: CreatePostUseCaseRequest
+  ): Promise<CreatePostUseCaseResponse> {
+    const newPost = Post.create({
+      authorId: post.authorId,
+      bloggersCommunityId: post.bloggersCommunityId,
+      content: post.content,
+      title: post.title,
+      topics: post.topics,
+    });
+
+    await this.postRepository.save(newPost);
+
+    return { post: newPost };
   }
 }
---
+++ src/domain/blog/app/use-cases/post/update-post-use-case.ts
@@ -0,0 +1,32 @@
+import { Post } from "@/domain/blog/enterprise/entities/post";
+import { PostRepository } from "../../repositories/post-repostitory";
+import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
+
+interface UpdatePostUseCaseRequest {
+  id: string;
+  title: string;
+  content: string;
+  topics: string[];
+}
+
+export class UpdatePostUseCase {
+  constructor(private readonly postRepository: PostRepository) {}
+  async execute({
+    id,
+    content,
+    title,
+    topics,
+  }: UpdatePostUseCaseRequest): Promise<void> {
+    const post = await this.postRepository.getById(id);
+
+    if (!post) {
+      throw new ResourceNotFoundError();
+    }
+
+    post.content = content;
+    post.title = title;
+    post.topics = topics;
+
+    await this.postRepository.update(post);
+  }
+}
