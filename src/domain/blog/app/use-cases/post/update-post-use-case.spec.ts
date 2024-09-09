dfae98a021fb777433e01e975757d7e74af9cff2
brunorg21 <rbrunog21@gmail.com>
2024-09-08 22:52:22 -0300

    feat: add verify to update post

--- src/domain/blog/app/use-cases/post/update-post-use-case.spec.ts
+++ src/domain/blog/app/use-cases/post/update-post-use-case.spec.ts
@@ -3,14 +3,30 @@ import { describe, expect, it } from "vitest";
 import { UpdatePostUseCase } from "./update-post-use-case";
 import { Post } from "@/domain/blog/enterprise/entities/post";
 import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
+import { InMemoryBloggerRepository } from "test/in-memory-repositories/in-memory-blogger-repository";
+import { NotAllowedError } from "../@errors/not-allowed-error";
+import { Blogger } from "@/domain/blog/enterprise/entities/blogger";
 
 describe("update post", () => {
   it("should be able to update a post", async () => {
     const repository = new InMemoryPostRepository();
-    const useCase = new UpdatePostUseCase(repository);
+    const bloggersRepository = new InMemoryBloggerRepository();
+    const useCase = new UpdatePostUseCase(repository, bloggersRepository);
+
+    const user = Blogger.create({
+      avatarUrl: null,
+      bloggersCommunityId: null,
+      createdAt: new Date(),
+      email: "user-email",
+      name: "user",
+      password: "123456",
+      role: "COMMON",
+    });
+
+    await bloggersRepository.save(user);
 
     const post = Post.create({
-      authorId: "author-id",
+      authorId: user.id,
       bloggersCommunityId: null,
       content: "teste",
       title: "Teste",
@@ -20,10 +36,11 @@ describe("update post", () => {
     await repository.save(post);
 
     await useCase.execute({
-      id: post._id,
+      id: post.id,
       content: "new content",
       title: "new title",
       topics: ["topic 1"],
+      authorId: user.id,
     });
 
     expect(repository.posts[0].content).toEqual("new content");
@@ -31,7 +48,8 @@ describe("update post", () => {
 
   it("should not be able to update a post with invalid id", async () => {
     const repository = new InMemoryPostRepository();
-    const useCase = new UpdatePostUseCase(repository);
+    const bloggersRepository = new InMemoryBloggerRepository();
+    const useCase = new UpdatePostUseCase(repository, bloggersRepository);
 
     const post = Post.create({
       authorId: "author-id",
@@ -50,7 +68,35 @@ describe("update post", () => {
           content: "new content",
           title: "new title",
           topics: ["topic 1"],
+          authorId: "author-id",
         })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to update a post you don't own", async () => {
+    const repository = new InMemoryPostRepository();
+    const bloggersRepository = new InMemoryBloggerRepository();
+    const useCase = new UpdatePostUseCase(repository, bloggersRepository);
+
+    const post = Post.create({
+      authorId: "author-id",
+      bloggersCommunityId: null,
+      content: "teste",
+      title: "Teste",
+      topics: [],
+    });
+
+    await repository.save(post);
+
+    expect(
+      async () =>
+        await useCase.execute({
+          id: post.id,
+          content: "new content",
+          title: "new title",
+          topics: ["topic 1"],
+          authorId: "author-id2",
+        })
+    ).rejects.toBeInstanceOf(NotAllowedError);
   });
 });
--- src/domain/blog/app/use-cases/post/update-post-use-case.ts
+++ src/domain/blog/app/use-cases/post/update-post-use-case.ts
@@ -1,4 +1,6 @@
+import { BloggerRepository } from "../../repositories/blogger-repository";
 import { PostRepository } from "../../repositories/post-repostitory";
+import { NotAllowedError } from "../@errors/not-allowed-error";
 import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
 
 interface UpdatePostUseCaseRequest {
@@ -6,20 +8,31 @@ interface UpdatePostUseCaseRequest {
   title: string;
   content: string;
   topics: string[];
+  authorId: string;
 }
 
 export class UpdatePostUseCase {
-  constructor(private readonly postRepository: PostRepository) {}
+  constructor(
+    private readonly postRepository: PostRepository,
+    private readonly bloggerRepository: BloggerRepository
+  ) {}
   async execute({
     id,
     content,
     title,
     topics,
+    authorId,
   }: UpdatePostUseCaseRequest): Promise<void> {
     const post = await this.postRepository.getById(id);
 
     if (!post) {
       throw new ResourceNotFoundError();
+    }
+
+    const blogger = await this.bloggerRepository.getById(authorId);
+
+    if (blogger?.role !== "ADMIN" && post.authorId !== blogger?.id) {
+      throw new NotAllowedError();
     }
 
     post.content = content;
