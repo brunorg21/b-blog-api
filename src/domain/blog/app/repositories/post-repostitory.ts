a306e84d7958f8661ccddc72d917e46d055f3bf0
brunorg21 <rbrunog21@gmail.com>
2024-09-16 17:20:19 -0300

    refactor: change getAll to receive paginated params

--- src/domain/blog/app/repositories/post-repostitory.ts
+++ src/domain/blog/app/repositories/post-repostitory.ts
@@ -1,9 +1,10 @@
+import { PaginatedParams } from "@/core/params";
 import { Post } from "../../enterprise/entities/post";
 
 export interface PostRepository {
   save(post: Post): Promise<void>;
   getById(id: string): Promise<Post | null>;
-  getAll(): Promise<Post[]>;
+  getAll(params: PaginatedParams, topic?: string): Promise<Post[]>;
   update(post: Post): Promise<void>;
   delete(post: Post): Promise<void>;
 }
