5039cf181970cb3f12240903e9a38fed99804bd1
brunorg21 <rbrunog21@gmail.com>
2024-09-25 16:29:17 -0300

    refactor: change post comment to extends comment entity

--- src/domain/blog/enterprise/entities/post-comment.ts
+++ src/domain/blog/enterprise/entities/post-comment.ts
@@ -1,51 +1,12 @@
-import { randomUUID } from "crypto";
+import { Comment, CommentProps } from "./comment";
 
-interface PostCommentProps {
+interface PostCommentProps extends CommentProps {
   postId: string;
-  authorId: string;
-  content: string;
-  createdAt: Date;
-  updatedAt: Date | null;
 }
 
-export class PostComment {
-  protected id: string;
-  constructor(private props: PostCommentProps, postCommentId?: string) {
-    this.props = props;
-    if (postCommentId) {
-      this.id = postCommentId;
-    }
-
-    this.id = randomUUID();
-  }
-
-  get _id() {
-    return this.id;
-  }
-  get content() {
-    return this.props.content;
-  }
-  get authorId() {
-    return this.props.authorId;
-  }
-
-  get createdAt() {
-    return this.props.createdAt;
-  }
-
+export class PostComment extends Comment<PostCommentProps> {
   get postId() {
     return this.props.postId;
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-
-  set content(content: string) {
-    this.props.content = content;
-  }
-  set authorId(authorId: string) {
-    this.props.authorId = authorId;
   }
 
   static create(
@@ -54,11 +15,9 @@ export class PostComment {
   ) {
     const postComment = new PostComment(
       {
-        authorId: props.authorId,
-        content: props.content,
+        ...props,
         createdAt: new Date(),
         updatedAt: null,
-        postId: props.postId,
       },
       id
     );
