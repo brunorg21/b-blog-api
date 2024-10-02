d54a7836d6c947d090e8191263abe679ff06c747
brunorg21 <rbrunog21@gmail.com>
2024-10-01 22:09:52 -0300

    feat: add getByPost method on repository interface

--- src/domain/blog/app/repositories/post-comment-repository.ts
+++ src/domain/blog/app/repositories/post-comment-repository.ts
@@ -1,9 +1,10 @@
+import { PaginatedParams } from "@/core/params";
 import { PostComment } from "../../enterprise/entities/post-comment";
-import { PostCommentLike } from "../../enterprise/entities/post-comment-like";
 
 export interface PostCommentRepository {
   save(postComment: PostComment): Promise<void>;
   getById(postCommentId: string): Promise<PostComment | null>;
+  getByPost(params: PaginatedParams, postId: string): Promise<PostComment[]>;
   update(postComment: PostComment): Promise<void>;
   delete(postComment: PostComment): Promise<void>;
 }
