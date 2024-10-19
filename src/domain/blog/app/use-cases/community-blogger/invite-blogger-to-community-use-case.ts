22929cabc7f1013f6efa4e7a197fd738173a23cd
brunorg21 <rbrunog21@gmail.com>
2024-10-19 00:01:32 -0300

    feat: add new use case to invite blogger

---
+++ src/domain/blog/app/use-cases/community-blogger/invite-blogger-to-community-use-case.ts
@@ -0,0 +1,50 @@
+import { CommunityBloggerRepository } from "../../repositories/community-blogger-repository";
+import { CommunityBlogger } from "@/domain/blog/enterprise/entities/community-blogger";
+import { BloggerAlreadyMemberOfCommunityError } from "../@errors/blogger-already-member-of-community-error";
+import { BloggerRepository } from "../../repositories/blogger-repository";
+
+interface InviteBloggerToCommunityUseCaseRequest {
+  bloggerId: string;
+  bloggerCommunityId: string;
+}
+
+interface InviteBloggerToCommunityUseCaseResponse {
+  communityBlogger: CommunityBlogger;
+}
+
+export class InviteBloggerToCommunityUseCase {
+  constructor(
+    private readonly communityBloggerRepository: CommunityBloggerRepository,
+    private readonly bloggerRepository: BloggerRepository
+  ) {}
+  async execute(
+    communityBlogger: InviteBloggerToCommunityUseCaseRequest
+  ): Promise<InviteBloggerToCommunityUseCaseResponse> {
+    const communitiesByBlogger =
+      await this.communityBloggerRepository.getAllByBloggerId(
+        communityBlogger.bloggerId
+      );
+
+    const alreadyMemberOfCommunity = communitiesByBlogger.find(
+      (community) =>
+        community.bloggerCommunityId === communityBlogger.bloggerCommunityId
+    );
+
+    if (alreadyMemberOfCommunity) {
+      const blogger = await this.bloggerRepository.getById(
+        communityBlogger.bloggerId
+      );
+
+      throw new BloggerAlreadyMemberOfCommunityError(blogger?.name ?? "");
+    }
+
+    const newCommunityBlogger = CommunityBlogger.create({
+      bloggerCommunityId: communityBlogger.bloggerCommunityId,
+      bloggerId: communityBlogger.bloggerId,
+    });
+
+    await this.communityBloggerRepository.save(newCommunityBlogger);
+
+    return { communityBlogger: newCommunityBlogger };
+  }
+}
