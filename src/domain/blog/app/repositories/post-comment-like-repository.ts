584d150b8ad7c2ec48ebbdcaf9b78766b22a4145
brunorg21 <rbrunog21@gmail.com>
2024-10-18 23:43:54 -0300

    remove files

--- src/domain/blog/app/repositories/post-comment-like-repository.ts
+++
@@ -1,9 +0,0 @@
-import { PostCommentLike } from "../../enterprise/entities/post-comment-like";
-
-export interface PostCommentLikeRepository {
-  save(postCommentLike: PostCommentLike): Promise<void>;
-  getById(postCommentLikeId: string): Promise<PostCommentLike | null>;
-  getByCommentId(postCommentLikeId: string): Promise<PostCommentLike | null>;
-  getAll(): Promise<PostCommentLike[]>;
-  delete(postCommentLike: PostCommentLike): Promise<void>;
-}
--- src/domain/blog/app/repositories/post-like-repository.ts
+++
@@ -1,9 +0,0 @@
-import { PostLike } from "../../enterprise/entities/post-like";
-
-export interface PostLikeRepository {
-  save(postLike: PostLike): Promise<void>;
-  getByPostId(postId: string): Promise<PostLike | null>;
-  getById(id: string): Promise<PostLike | null>;
-  getAll(): Promise<PostLike[]>;
-  delete(postLike: PostLike): Promise<void>;
-}
