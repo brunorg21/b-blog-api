4f85cde4fac6c6ed3b36071716f995d07c42e913
brunorg21 <rbrunog21@gmail.com>
2024-10-19 00:03:27 -0300

    feat: add new method on notification repository

--- src/domain/notification/app/repositories/notification-repository.ts
+++ src/domain/notification/app/repositories/notification-repository.ts
@@ -4,4 +4,5 @@ export interface NotificationRepository {
   create(notification: Notification): Promise<void>;
   save(notification: Notification): Promise<void>;
   getById(notificationId: string): Promise<Notification | null>;
+  delete(notification: Notification): Promise<void>;
 }
