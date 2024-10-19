5a0fb76f99334db71e2fd39add3b8dcc8243df1b
brunorg21 <rbrunog21@gmail.com>
2024-10-19 00:02:19 -0300

    feat: add community blogger entity

---
+++ src/domain/blog/enterprise/entities/community-blogger.ts
@@ -0,0 +1,49 @@
+import { generateSlug } from "@/core/generate-slug";
+import { randomUUID } from "node:crypto";
+
+interface CommunityBloggerProps {
+  bloggerId: string;
+  bloggerCommunityId: string;
+}
+
+export class CommunityBlogger {
+  private _id: string;
+  private props: CommunityBloggerProps;
+
+  constructor(props: CommunityBloggerProps, id?: string) {
+    this._id = id ?? randomUUID();
+    this.props = props;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get bloggerId() {
+    return this.props.bloggerId;
+  }
+
+  get bloggerCommunityId() {
+    return this.props.bloggerCommunityId;
+  }
+
+  set bloggerId(bloggerId: string) {
+    this.props.bloggerId = bloggerId;
+  }
+
+  set bloggerCommunityId(bloggerCommunityId: string) {
+    this.props.bloggerCommunityId = bloggerCommunityId;
+  }
+
+  static create(props: CommunityBloggerProps, id?: string) {
+    const bloggerscommunity = new CommunityBlogger(
+      {
+        bloggerCommunityId: props.bloggerCommunityId,
+        bloggerId: props.bloggerId,
+      },
+      id
+    );
+
+    return bloggerscommunity;
+  }
+}
