84ce1e58f69feba70f1420b1dcd0862f33696747
brunorg21 <rbrunog21@gmail.com>
2024-10-28 22:19:15 -0300

    feat: add fastify bootstrap

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,1 +1,10 @@
+import fastify from "fastify";
 
+const app = fastify();
+
+app
+  .listen({
+    port: 3000,
+    host: "0.0.0.0",
+  })
+  .then(() => console.log("Server running on port 3000"));
