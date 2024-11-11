9e9c0c08500b7a6cd2bf982c31821c75a92beace
brunorg21 <rbrunog21@gmail.com>
2024-11-11 14:04:19 -0300

    refactor: add findById method on update blogger use case

--- src/domain/blog/app/use-cases/blogger/update-blogger-use-case.ts
+++ src/domain/blog/app/use-cases/blogger/update-blogger-use-case.ts
@@ -1,11 +1,9 @@
 import { Blogger } from "@/domain/blog/enterprise/entities/blogger";
 import { BloggerRepository } from "../../repositories/blogger-repository";
-import { UserAlreadyExistsError } from "../@errors/user-already-exists-error";
-
-import { hash } from "bcrypt";
 import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
 
 interface UpdateBloggerUseCaseRequest {
+  id: string;
   avatarUrl: string | null;
   email: string;
   name: string;
@@ -18,11 +16,12 @@ interface UpdateBloggerUseCaseResponse {
 export class UpdateBloggerUseCase {
   constructor(private readonly bloggerRepository: BloggerRepository) {}
   async execute({
+    id,
     avatarUrl,
     email,
     name,
   }: UpdateBloggerUseCaseRequest): Promise<UpdateBloggerUseCaseResponse> {
-    const blogger = await this.bloggerRepository.findByEmail(email);
+    const blogger = await this.bloggerRepository.findById(id);
 
     if (!blogger) {
       throw new ResourceNotFoundError();
