bfa1e275d8b85ae76158cbd00330dce59d159344
brunorg21 <rbrunog21@gmail.com>
2024-11-13 17:08:05 -0300

    feat: add topics on request interface

--- src/domain/blog/app/use-cases/post/update-post-use-case.ts
+++ src/domain/blog/app/use-cases/post/update-post-use-case.ts
@@ -1,3 +1,4 @@
+import { PostTopic } from "@/domain/blog/enterprise/entities/topic-post";
 import { BloggerRepository } from "../../repositories/blogger-repository";
 import { PostRepository } from "../../repositories/post-repostitory";
 import { PostTopicsRepository } from "../../repositories/post-topics-repository";
@@ -8,7 +9,7 @@ interface UpdatePostUseCaseRequest {
   id: string;
   title: string;
   content: string;
-
+  topics: string[];
   authorId: string;
 }
 
@@ -22,14 +23,45 @@ export class UpdatePostUseCase {
     id,
     content,
     title,
-
     authorId,
+    topics,
   }: UpdatePostUseCaseRequest): Promise<void> {
     const post = await this.postRepository.getById(id);
 
     if (!post) {
       throw new ResourceNotFoundError();
     }
+
+    if (topics.length > 0) {
+      const topicsExists = await this.postTopicRepository.getByPostId(post.id);
+
+      const topicsToBeDeleted = topicsExists?.filter(
+        (topic) => !topics.includes(topic.topicId)
+      );
+
+      if (topicsToBeDeleted)
+        await Promise.all([
+          topicsToBeDeleted.map((topic) =>
+            this.postTopicRepository.delete(topic)
+          ),
+        ]);
+
+      const topicsToBeAdded = topics.filter(
+        (topicId) => !topicsExists?.some((t) => t.topicId === topicId)
+      );
+
+      topicsToBeAdded.map(
+        async (topicId) =>
+          await this.postTopicRepository.save(
+            PostTopic.create({
+              postId: post.id,
+              topicId,
+            })
+          )
+      );
+    }
+
+    const editedTopics = await this.postTopicRepository.getByPostId(post.id);
 
     const blogger = await this.bloggerRepository.getById(authorId);
 
@@ -39,6 +71,7 @@ export class UpdatePostUseCase {
 
     post.content = content;
     post.title = title;
+    post.topics = editedTopics.map((topic) => topic.topicId);
 
     await this.postRepository.update(post);
   }
