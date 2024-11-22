db2bd8449b010518ff3075fac4b7fc40fa4f5965
brunorg21 <rbrunog21@gmail.com>
2024-11-21 23:33:59 -0300

    refactor: change to environment variables

--- src/infra/database/typeorm/index.ts
+++ src/infra/database/typeorm/index.ts
@@ -3,17 +3,18 @@ import { DataSource } from "typeorm";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import "reflect-metadata";
+import { env } from "@/env";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const appDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "docker",
-  password: "docker",
-  database: "blog-api-db",
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  username: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [`${__dirname}/**/schemas/*.{ts,js}`],
