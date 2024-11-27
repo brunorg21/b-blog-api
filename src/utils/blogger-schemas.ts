1a5cbb617a327f20ee0d3841e7e59ed264c1bccb
brunorg21 <rbrunog21@gmail.com>
2024-11-27 16:17:20 -0300

    refactor: change schema props type

--- src/utils/blogger-schemas.ts
+++ src/utils/blogger-schemas.ts
@@ -9,12 +9,12 @@ export const registerSchema = z.object({
   name: z.string().max(50),
   email: z.string().email(),
   password: z.string(),
-  avatarUrl: z.string().nullable(),
+  avatarUrl: z.string().nullable().default(null),
   role: z.enum(["ADMIN", "COMMON"]).default("COMMON"),
 });
 export const updateBloggerSchema = z.object({
   name: z.string().max(50),
   email: z.string().email(),
-  avatarUrl: z.string().nullable(),
+  avatarUrl: z.string().nullable().default(null),
   role: z.enum(["ADMIN", "COMMON"]).default("COMMON"),
 });
--- src/utils/post-schemas.ts
+++ src/utils/post-schemas.ts
@@ -3,13 +3,13 @@ import { z } from "zod";
 export const createPostSchema = z.object({
   content: z.string().max(500).min(15),
   topics: z.array(z.string()).default([]).optional(),
-  bloggerCommunityId: z.string().nullable(),
+  bloggerCommunityId: z.string().nullable().default(null),
   title: z.string(),
 });
 
 export const updatePostSchema = z.object({
   content: z.string().max(500).min(15),
-  topics: z.array(z.string()),
+  topics: z.array(z.string()).default([]).optional(),
   title: z.string(),
 });
 
@@ -18,5 +18,5 @@ export const paramsPostSchema = z.object({
 });
 
 export const queryPostSchema = z.object({
-  page: z.number().default(10).optional(),
+  page: z.coerce.number().default(1).optional(),
 });
