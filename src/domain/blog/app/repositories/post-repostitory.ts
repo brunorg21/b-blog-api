17ecaf67a3e960660b81c03685768e5bb16ec107
brunorg21 <rbrunog21@gmail.com>
2024-11-27 16:07:17 -0300

    refactor: change method on post repository

--- src/domain/blog/app/repositories/post-repostitory.ts
+++ src/domain/blog/app/repositories/post-repostitory.ts
@@ -1,12 +1,14 @@
 import { PaginatedParams } from "@/core/params";
 import { Post } from "../../enterprise/entities/post";
 import { PostDetails } from "../../enterprise/entities/value-objects/post-with-details";
+import { PostWithComments } from "../../enterprise/entities/value-objects/post-with-comments";
 
 export interface PostRepository {
-  save(post: Post): Promise<void>;
+  save(post: Post): Promise<Post>;
   getById(id: string): Promise<Post | null>;
   getAll(params: PaginatedParams): Promise<Post[]>;
   update(post: Post): Promise<void>;
   delete(post: Post): Promise<void>;
   getPostsWithDetails(params: PaginatedParams): Promise<PostDetails[]>;
+  getPostWithComments(id: string): Promise<PostWithComments | null>;
 }
