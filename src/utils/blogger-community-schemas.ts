2dea9b1bc38c97d05ed5836b750970483b492fb6
brunorg21 <rbrunog21@gmail.com>
2024-12-08 22:58:42 -0300

    feat: add blogger community schema

---
+++ src/utils/blogger-community-schemas.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const createBloggerCommunitySchema = z.object({
+  description: z.string(),
+  name: z.string(),
+  avatarUrl: z.string().nullable().default(null),
+});
+
+export const updateBloggerCommunitySchema = z.object({
+  description: z.string().max(500).min(15),
+  name: z.string(),
+});
+
+export const paramsBloggersCommunitySchema = z.object({
+  id: z.string(),
+});
+
+export const queryPostSchema = z.object({
+  page: z.coerce.number().default(1).optional(),
+});
