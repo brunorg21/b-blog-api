d5ff697a006ea56a07c8f28e9e6676414972a6b7
brunorg21 <rbrunog21@gmail.com>
2024-12-08 22:52:55 -0300

    refactor: change generateSlug function

--- src/core/generate-slug.ts
+++ src/core/generate-slug.ts
@@ -1,11 +1,5 @@
 export function generateSlug(text: string, hasPrefix: boolean = true): string {
-  const slug = text
-    .toLowerCase()
-    .trim()
-    .normalize("NFD")
-    .replace(/[\u0300-\u036f]/g, "")
-    .replace(/[^a-z0-9\s-]/g, "")
-    .replace(/\s+/g, "-");
+  const slug = text.trim().toLowerCase().replace(/\s+/g, "-");
 
   return hasPrefix ? `b/${slug}` : slug;
 }
