f9c442e73198f29de0d44c5bd39bcbd6b62e68b3
brunorg21 <rbrunog21@gmail.com>
2024-12-10 22:08:47 -0300

    feat: add new method on interface repository

--- src/domain/blog/app/repositories/bloggers-community-repository.ts
+++ src/domain/blog/app/repositories/bloggers-community-repository.ts
@@ -2,7 +2,7 @@ import { BloggersCommunity } from "../../enterprise/entities/bloggers-community"
 
 export interface BloggersCommunityRepository {
   save(bloggersCommunity: BloggersCommunity): Promise<void>;
-  getById(bloggerId: string): Promise<BloggersCommunity | null>;
+  getById(bloggerCommunityId: string): Promise<BloggersCommunity | null>;
   update(bloggersCommunity: BloggersCommunity): Promise<void>;
   delete(bloggersCommunity: BloggersCommunity): Promise<void>;
   getAllByAuthorId(bloggerId: string): Promise<BloggersCommunity[]>;
