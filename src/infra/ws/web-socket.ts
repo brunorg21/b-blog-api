09d0316f9dd1951eaace2dedb4ab336214447851
brunorg21 <rbrunog21@gmail.com>
2024-12-10 22:13:37 -0300

    remove imports

--- src/infra/ws/web-socket.ts
+++ src/infra/ws/web-socket.ts
@@ -1,9 +1,7 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { TypeormBloggerRepository } from "../database/typeorm/repositories/typeorm-blogger-repository";
-import { OnInviteBloggerToCommunitySubscriber } from "@/domain/notification/app/subscribers/on-invite-blogger-to-community";
 import { TypeormBloggerCommunityRepository } from "../database/typeorm/repositories/typeorm-bloggers-community-repository";
-import { SendNotificationUseCase } from "@/domain/notification/app/use-cases/send-notification";
 import { TypeormNotificationRepository } from "../database/typeorm/repositories/typeorm-notification-repository";
 import { OnInviteToBloggerCommunityEvent } from "./events/on-invite-to-blogger-community-event";
 
