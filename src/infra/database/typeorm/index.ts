9a85d7b7acc3ff7b47fa15c7cf83dad477e7e117
brunorg21 <rbrunog21@gmail.com>
2024-12-12 00:19:29 -0300

    refactor: add entities on data source config

--- src/infra/database/typeorm/index.ts
+++ src/infra/database/typeorm/index.ts
@@ -4,6 +4,16 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 import "reflect-metadata";
 import { env } from "@/env";
+import { BloggerEntity } from "./schemas/blogger";
+import { BloggerCommunityEntity } from "./schemas/blogger-community";
+import { CommentLikeEntity } from "./schemas/comment-likes";
+import { CommentEntity } from "./schemas/comment";
+import { CommunityBloggerEntity } from "./schemas/community-blogger";
+import { NotificationEntity } from "./schemas/notification";
+import { PostLikeEntity } from "./schemas/post-likes";
+import { PostTopicsEntity } from "./schemas/post-topics";
+import { PostEntity } from "./schemas/post";
+import { TopicEntity } from "./schemas/topic";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,8 +25,19 @@ export const appDataSource = new DataSource({
   username: env.DB_USER,
   password: env.DB_PASSWORD,
   database: env.DB_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
-  entities: [`${__dirname}/**/schemas/*.{ts,js}`],
+  entities: [
+    BloggerEntity,
+    BloggerCommunityEntity,
+    CommentLikeEntity,
+    CommentEntity,
+    CommunityBloggerEntity,
+    NotificationEntity,
+    PostLikeEntity,
+    PostTopicsEntity,
+    PostEntity,
+    TopicEntity,
+  ],
   migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 });
