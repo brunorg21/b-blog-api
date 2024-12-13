4de2aa5d69b10046425cb3dccd5fa61ce017ec09
brunorg21 <rbrunog21@gmail.com>
2024-12-13 16:44:42 -0300

    remove: delete method on repository

--- src/domain/blog/app/repositories/community-blogger-repository.ts
+++ src/domain/blog/app/repositories/community-blogger-repository.ts
@@ -3,8 +3,4 @@ import { CommunityBlogger } from "../../enterprise/entities/community-blogger";
 export interface CommunityBloggerRepository {
   save(communityBlogger: CommunityBlogger): Promise<void>;
   getAllByBloggerId(bloggerId: string): Promise<CommunityBlogger[]>;
-  getByBloggerIdAndBloggerCommunityId(
-    bloggerId: string,
-    bloggerCommunityId: string
-  ): Promise<CommunityBlogger | null>;
 }
