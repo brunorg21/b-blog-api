ccc014d83ec91550e8522676b6191bd8b669ff92
brunorg21 <rbrunog21@gmail.com>
2025-03-18 15:52:30 -0300

    remove: delete methods on blogger community interface

--- src/infra/http/controllers/interfaces/blogger-community-interface.ts
+++ src/infra/http/controllers/interfaces/blogger-community-interface.ts
@@ -1,4 +1,5 @@
 import { BloggersCommunity } from "@/domain/blog/enterprise/entities/bloggers-community";
+import { BloggerCommunityWithPosts } from "@/domain/blog/enterprise/entities/value-objects/blogger-community-with-posts";
 
 export interface UpdateBloggersCommunityRequest {
   id: string;
@@ -10,13 +11,12 @@ export interface BloggerCommunityControllerInterface {
   getBloggerCommunitiesByAuthor(
     bloggerId: string
   ): Promise<BloggersCommunity[]>;
-  getUniqueBloggersCommunity(id: string): Promise<BloggersCommunity>;
   getAllBloggersCommunity(bloggerId: string): Promise<BloggersCommunity[]>;
   updateBloggerCommunity(
     data: UpdateBloggersCommunityRequest,
     bloggerId: string
   ): void;
-  getBySlug(slug: string): Promise<BloggersCommunity>;
+  getBySlug(slug: string): Promise<BloggerCommunityWithPosts>;
   getBloggerCommunitiesByBlogger(
     bloggerId: string
   ): Promise<BloggersCommunity[]>;
