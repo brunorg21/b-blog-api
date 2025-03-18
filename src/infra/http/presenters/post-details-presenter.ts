116f75f40c5b8f551b343a9e7e6d814b3e143b7d
brunorg21 <rbrunog21@gmail.com>
2025-03-18 15:53:49 -0300

    refactor: use presenter to format data

--- src/infra/http/presenters/post-details-presenter.ts
+++ src/infra/http/presenters/post-details-presenter.ts
@@ -1,5 +1,6 @@
 import { PostDetails } from "@/domain/blog/enterprise/entities/value-objects/post-with-details";
 import { TopicPresenter } from "./topic-presenter";
+import { BloggersCommunityPresenter } from "./bloggers-community-presenter";
 
 export class PostDetailsPresenter {
   static toHTTP(post: PostDetails) {
@@ -11,8 +12,10 @@ export class PostDetailsPresenter {
       createdAt: post.createdAt,
       updatedAt: post.updatedAt,
       likeCount: post.likeCount,
-      bloggerCommunity: post.bloggerCommunity,
-      postTopics: post.topics.map(TopicPresenter.toHTTP),
+      bloggerCommunity: post.bloggerCommunity
+        ? BloggersCommunityPresenter.toHTTP(post.bloggerCommunity)
+        : null,
+      postTopics: post?.topics?.map(TopicPresenter.toHTTP),
       author: post.author,
     };
   }
