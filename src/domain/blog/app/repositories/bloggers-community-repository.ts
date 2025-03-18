33d6c6171ab95c47b760b74e3c87ad9692693779
brunorg21 <rbrunog21@gmail.com>
2025-03-18 15:41:42 -0300

    refactor: change repository interface

--- src/domain/blog/app/repositories/bloggers-community-repository.ts
+++ src/domain/blog/app/repositories/bloggers-community-repository.ts
@@ -1,12 +1,13 @@
 import { BloggersCommunity } from "../../enterprise/entities/bloggers-community";
+import { BloggerCommunityWithPosts } from "../../enterprise/entities/value-objects/blogger-community-with-posts";
 
 export interface BloggersCommunityRepository {
-  save(bloggersCommunity: BloggersCommunity): Promise<void>;
+  save(bloggersCommunity: BloggersCommunity): Promise<BloggersCommunity>;
   getById(bloggerCommunityId: string): Promise<BloggersCommunity | null>;
   update(bloggersCommunity: BloggersCommunity): Promise<void>;
   delete(bloggersCommunity: BloggersCommunity): Promise<void>;
   getAllByAuthorId(bloggerId: string): Promise<BloggersCommunity[]>;
   getAll(): Promise<BloggersCommunity[]>;
-  getBySlug(slug: string): Promise<BloggersCommunity | null>;
+  getBySlug(slug: string): Promise<BloggerCommunityWithPosts | null>;
   getByIds(bloggerCommunityIds: string[]): Promise<BloggersCommunity[]>;
 }
