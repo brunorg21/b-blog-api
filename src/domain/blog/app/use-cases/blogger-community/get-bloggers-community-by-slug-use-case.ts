33ace6bc393911fada5ab42ea0e80ab0178f9603
brunorg21 <rbrunog21@gmail.com>
2025-04-14 20:57:21 -0300

    refactor: add paginantion on get bloggers

--- src/domain/blog/app/use-cases/blogger-community/get-bloggers-community-by-slug-use-case.ts
+++ src/domain/blog/app/use-cases/blogger-community/get-bloggers-community-by-slug-use-case.ts
@@ -2,9 +2,11 @@ import { BloggersCommunity } from "@/domain/blog/enterprise/entities/bloggers-co
 import { BloggersCommunityRepository } from "../../repositories/bloggers-community-repository";
 import { ResourceNotFoundError } from "../@errors/resource-not-found-error";
 import { BloggerCommunityWithPosts } from "@/domain/blog/enterprise/entities/value-objects/blogger-community-with-posts";
+import { PaginatedParams } from "@/core/params";
 
 interface GetBloggersCommunityBySlugUseCaseRequest {
   slug: string;
+  params: PaginatedParams;
 }
 
 interface GetBloggersCommunityBySlugUseCaseResponse {
@@ -17,9 +19,11 @@ export class GetBloggersCommunityBySlugUseCase {
   ) {}
   async execute({
     slug,
+    params,
   }: GetBloggersCommunityBySlugUseCaseRequest): Promise<GetBloggersCommunityBySlugUseCaseResponse> {
     const bloggersCommunity = await this.bloggersCommunityRepository.getBySlug(
-      slug
+      slug,
+      params
     );
 
     if (!bloggersCommunity) {
