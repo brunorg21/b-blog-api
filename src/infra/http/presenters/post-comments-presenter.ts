2051d7176568b9d0172598bc6b68981568900155
brunorg21 <rbrunog21@gmail.com>
2025-04-14 20:59:35 -0300

    refactor: change bloggerCommunity format

--- src/infra/http/presenters/post-comments-presenter.ts
+++ src/infra/http/presenters/post-comments-presenter.ts
@@ -1,6 +1,7 @@
 import { PostWithComments } from "@/domain/blog/enterprise/entities/value-objects/post-with-comments";
 import { TopicPresenter } from "./topic-presenter";
 import { CommentDetailsPresenter } from "./comment-details-presenter";
+import { BloggersCommunityPresenter } from "./bloggers-community-presenter";
 
 export class PostWithCommentsPresenter {
   static toHTTP(post: PostWithComments) {
@@ -12,7 +13,9 @@ export class PostWithCommentsPresenter {
       createdAt: post.createdAt,
       updatedAt: post.updatedAt,
       likeCount: post.likeCount,
-      bloggerCommunity: post.bloggerCommunity,
+      bloggerCommunity: post.bloggerCommunity
+        ? BloggersCommunityPresenter.toHTTP(post.bloggerCommunity)
+        : null,
       postTopics: post.postTopics.map(TopicPresenter.toHTTP),
       author: post.author,
       comments: post.comments.map(CommentDetailsPresenter.toHTTP),
